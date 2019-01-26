870a6bfaecc050da15a7459befe5c1cc545a2581
kamerican <44129995+kamerican@users.noreply.github.com>
2019-01-26 17:55:53 -0600

    Complete django poll tutorial

--- polls/models.py
+++ polls/models.py
@@ -6,22 +6,27 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def __repr__(self):
-        return "Question: {0}\nDate: {1}".format(
-            self.question_text,
-            self.pub_date,
-        )
+    def __str__(self):
+        return self.question_text
+    # def __repr__(self):
+    #     return "Question: {0}\nDate: {1}".format(
+    #         self.question_text,
+    #         self.pub_date,
+    #     )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __repr__(self):
-        return "Choice: {0} for the question: {1}\nNumber of votes: {2}".format(
-            self.choice_text,
-            self.question,
-            self.votes,
-        )
+    def __str__(self):
+        return self.choice_text
+    # def __repr__(self):
+    #     return "Choice: {0} for the question: {1}\nNumber of votes: {2}".format(
+    #         self.choice_text,
+    #         self.question,
+    #         self.votes,
+    #     )
